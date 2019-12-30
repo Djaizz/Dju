ccb1d9d19375dcf2722a2b69aef1579a70ed8a8e
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-12-30 16:58:53 +0700

    minor enhancement to django_util.autocompletes.autocomplete_factory

--- django_util/autocompletes.py
+++ django_util/autocompletes.py
@@ -4,18 +4,27 @@ from functools import reduce
 from operator import or_, __or__
 
 
-def autocomplete_factory(ModelClass, min_input_len=0):
+def autocomplete_factory(ModelClass, search_fields=(), min_input_len=0):
     class AutoComplete(Select2QuerySetView):
         __name__ = __qualname__ = name = \
             '{}AutoComplete'.format(ModelClass.__name__)
 
         data_min_input_len = min_input_len
 
+        autocomplete_search_fields = \
+            ['{}__icontains'.format(search_field)
+             for search_field in search_fields] \
+            if search_fields \
+            else (ModelClass.autocomplete_search_fields()
+                  if hasattr(ModelClass, 'autocomplete_search_fields')
+                  else ['{}__icontains'.format(search_field)
+                        for search_field in ModelClass.search_fields()])
+
         def get_queryset(self):
             return (ModelClass.objects.filter(
                         reduce(__or__,
                                (Q(**{autocomplete_search_field: self.q})
-                                for autocomplete_search_field in ModelClass.autocomplete_search_fields())))
+                                for autocomplete_search_field in self.autocomplete_search_fields)))
                     if self.q
                     else (ModelClass.objects.none()
                           if self.data_min_input_len
