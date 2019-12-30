a3b7b92ef1ccd75f61a4019c0d87aff1ba9d8adb
Vinh Luong <MBALearnsToCode@UChicago.edu>
2019-12-30 16:26:32 +0700

    minor fixes

--- django_util/__init__.py
+++ django_util/__init__.py
@@ -1,1 +1,20 @@
+import re
+
+
+# Django App Config
 default_app_config = 'django_util.apps.UtilConfig'
+
+
+MAX_CHAR_FLD_LEN = 255
+
+
+def _clean(s):
+    return re.sub('_{2,}', '_', re.sub('[^\w]+', '_', s).strip('_'))
+
+
+def snake_case(s):
+    return _clean(s).lower()
+
+
+def upper_case(s):
+    return _clean(s).upper()
